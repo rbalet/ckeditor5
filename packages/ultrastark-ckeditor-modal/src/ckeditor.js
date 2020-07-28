76af692360804a10e668cd5fb594a1cda3f70f9d
Raphael Balet <r.balet@ultrastark.ch>
2020-07-28 17:30:26 +0200

    add - template : modal

--- packages/ultrastark-ckeditor-modal/src/ckeditor.js
+++ packages/ultrastark-ckeditor-modal/src/ckeditor.js
@@ -7,17 +7,12 @@
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
 import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor";
-import Heading from "@ckeditor/ckeditor5-heading/src/heading";
-import Indent from "@ckeditor/ckeditor5-indent/src/indent";
+import Link from "@ckeditor/ckeditor5-link/src/link";
 import List from "@ckeditor/ckeditor5-list/src/list";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
-import Table from "@ckeditor/ckeditor5-table/src/table";
-import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
 import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
 
 export default class ClassicEditor extends ClassicEditorBase {}
@@ -25,18 +20,13 @@ export default class ClassicEditor extends ClassicEditorBase {}
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	Heading,
 	Autoformat,
-	FontColor,
 	Bold,
 	Italic,
-	BlockQuote,
-	Indent,
+	Link,
 	List,
 	Paragraph,
 	PasteFromOffice,
-	Table,
-	TableToolbar,
 	TextTransformation,
 ];
 
@@ -47,23 +37,14 @@ ClassicEditor.defaultConfig = {
 			"undo",
 			"redo",
 			"|",
-			"heading",
-			"|",
 			"bold",
 			"italic",
-			"fontColor",
 			"|",
 			"bulletedList",
 			"numberedList",
-			"indent",
-			"outdent",
 			"|",
-			"insertTable",
-			"blockQuote",
+			"link",
 		],
-	},
-	table: {
-		contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: "en",
