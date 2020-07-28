1711004a485f11759735b2c0e3f70a322b935472
Raphael Balet <r.balet@ultrastark.ch>
2020-07-28 18:34:15 +0200

    add - template : cloudBundle

--- packages/ultrastark-ckeditor-cloud-bundle/src/ckeditor.js
+++ packages/ultrastark-ckeditor-cloud-bundle/src/ckeditor.js
@@ -4,66 +4,76 @@
  */
 
 // The editor creator to use.
-import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
 import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
-import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
-import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
 import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-import FontBackgroundColor from "@ckeditor/ckeditor5-font/src/fontBackgroundColor";
 import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor";
 import Heading from "@ckeditor/ckeditor5-heading/src/heading";
-import Image from "@ckeditor/ckeditor5-image/src/image";
-import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
-import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
-import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
-import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
 import Indent from "@ckeditor/ckeditor5-indent/src/indent";
 import Link from "@ckeditor/ckeditor5-link/src/link";
 import List from "@ckeditor/ckeditor5-list/src/list";
-import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
 import Table from "@ckeditor/ckeditor5-table/src/table";
 import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
 import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
 
-export default class WebsiteEditors extends ClassicEditorBase {}
+class ModalEditor extends ClassicEditorBase {}
+class LetterEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
-ClassicEditor.builtinPlugins = [
+ModalEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
-	BlockQuote,
-	CKFinder,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
 	Link,
 	List,
-	MediaEmbed,
+	Paragraph,
+	PasteFromOffice,
+	TextTransformation,
+];
+
+LetterEditor.builtinPlugins = [
+	Essentials,
+	Heading,
+	Autoformat,
+	FontColor,
+	Bold,
+	Italic,
+	BlockQuote,
+	Indent,
+	List,
 	Paragraph,
 	PasteFromOffice,
 	Table,
 	TableToolbar,
 	TextTransformation,
-	FontColor,
-	FontBackgroundColor,
 ];
 
 // Editor configuration.
-ClassicEditor.defaultConfig = {
+ModalEditor.defaultConfig = {
+	toolbar: {
+		items: [
+			"undo",
+			"redo",
+			"|",
+			"bold",
+			"italic",
+			"|",
+			"bulletedList",
+			"numberedList",
+			"|",
+			"link",
+		],
+	},
+	// This value must be kept in sync with the language defined in webpack.config.js.
+	language: "en",
+};
+LetterEditor.defaultConfig = {
 	toolbar: {
 		items: [
 			"undo",
@@ -74,26 +84,14 @@ ClassicEditor.defaultConfig = {
 			"bold",
 			"italic",
 			"fontColor",
-			"fontBackgroundColor",
-			"link",
 			"|",
 			"bulletedList",
 			"numberedList",
 			"indent",
 			"outdent",
 			"|",
-			"imageUpload",
-			"blockQuote",
 			"insertTable",
-			"mediaEmbed",
-		],
-	},
-	image: {
-		toolbar: [
-			"imageStyle:full",
-			"imageStyle:side",
-			"|",
-			"imageTextAlternative",
+			"blockQuote",
 		],
 	},
 	table: {
@@ -101,4 +99,9 @@ ClassicEditor.defaultConfig = {
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: "en",
+};
+
+export default {
+	ModalEditor,
+	LetterEditor,
 };
